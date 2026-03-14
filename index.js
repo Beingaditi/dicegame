ee9cf4f41be6a34e0b3118eeeb85f1d87b52fd83
Aditi <aditijamgade@gmail.com>
2026-03-15 00:47:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,42 +2,6 @@
 
 
 
-/*var randomnum=(Math.floor(Math.random()*6))+1;
-
-
-var ran="dice"+randomnum+".png";
-var imgsrc="images/"+  ran;
-var img1=document.querySelectorAll("img")[0];
-
-img1.setAttribute("src",imgsrc);
-
-
-var randomnum2=(Math.floor(Math.random()*6))+1;
-
-
-var ran2="dice"+randomnum2+".png";
-var imgsrc2="images/"+  ran2;
-var img2=document.querySelectorAll("img")[1];
-
-img2.setAttribute("src",imgsrc2);
-
-
-
-
-if(ran>ran2){
-
-
-    document.querySelector("h1").innerHTML="Player 1 wins!!!";
-}
-else if(ran2>ran){
-
-    document.querySelector("h1").innerHTML="Player 2 wins!!!";
-} 
-else{
-
-     document.querySelector("h1").innerHTML="Draw";
-}*/
-
 
 
 
